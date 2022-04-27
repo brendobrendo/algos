4f8fe64830f70e2fc5fed0ba3ede0c8f73b87773
brendobrendo <brendan.smith903@gmail.com>
2022-04-27 11:31:19 -0700

    "BSTs/AVLs"

---
+++ Data-Structures/AVLTree.js
@@ -0,0 +1,17 @@
+/**
+ * INSERTION PSEUDOCODE
+ * 1) Insert the new element into the BST using BST insertion logic
+ * 2) check the balance factor for every node
+ * 3) If the balance factor for a node is 0, 1, or -1 then that node
+ * is balanced and you can move to the next one
+ * 4) Else, perform the suitable rotation to make it balanced
+ */
+
+/**
+ * DELETION PSEUDOCODE
+ * 1) Delete the specified element using BST deletion logic
+ * 2) Check the balance factor for every node
+ * 3) If the the balance factor for a node is 0, 1, or -1, then that node is
+ * balanced and you can move on to the next one
+ * 4) Else, perform the suitable rotation to make it balanced
+ */
