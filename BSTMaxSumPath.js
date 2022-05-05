49326d828041584e8364ba21f60361bc12e368d4
brendobrendo <brendan.smith903@gmail.com>
2022-05-04 15:39:48 -1000

    "not ready yet"

---
+++ BSTMaxSumPath.js
@@ -0,0 +1,40 @@
+// NOT FINISHED!!!!!
+
+// Create class for BST Node
+class BSTNode {
+    constructor(val=null, leftNode=null, rightNode=null) {
+        return this;
+    }
+}
+
+// Create class for Solution
+    // create function maxPathSum
+        // instantiate variable to return as our result
+
+        // Create edge case- if there are no nodes, return 0
+
+        // Create recursive calls for left and right side of the node
+
+class Solution {
+    /**
+     * Return the sum value of the nodes on the max path within a given binary tree
+     * @param {BSTNode | Null} root root node of tree we will be traversing
+     * @returns {Number} Sum of the nodes' values on the max path
+     */
+    maxSumPath(root=null) {
+        let res;
+
+        recursiveFunction = (rootNode=root) => {
+            // Base case
+            if (!rootNode) return 0;
+
+            // Recursive case
+            
+            return "not finished"
+
+        }
+    }
+}
+
+
+
