adf18803173d43b2b78a260315f2f0773c8a1add
brendobrendo <brendan.smith903@gmail.com>
2022-05-11 19:17:07 -1000

    Remove and HeapifyDown functionality

--- Data-Structures/MinHeap.js
+++ Data-Structures/MinHeap.js
@@ -22,7 +22,7 @@ class MinHeap {
      * null if there isn't a left child
      */
     leftChild(index) {
-        if (isLeaf(index)) return null;
+        if (this.isLeaf(index)) return null;
         return (index*2)+1;
     }
 
@@ -33,7 +33,7 @@ class MinHeap {
      * right child
      */
     rightChild(index) {
-        if (isLeaf(index)) return null;
+        if (this.isLeaf(index)) return null;
         return (index*2)+2;
     }
 
@@ -67,11 +67,84 @@ class MinHeap {
         
         // While val of current index is less than val at index of parent
         // swap the vals to maintain the min heap property
-        while (this.values[idx]>this.values[parentIdx]) {
+        while (this.values[idx]<this.values[parentIdx]) {
             this.swap(idx, parentIdx);
             [idx, parentIdx] = [parentIdx, this.parent(parentIdx)]
         }
 
         return this;
     }
+
+    /**
+     * @returns {Number | Null} returns and removes the min val from the heap
+     * or null if there are no values in the heap
+     */
+    removeMin = () => {
+        // First swap the min element with the last element in the array
+        this.swap(0, this.values.length-1);
+        // capture the min element with .pop() method
+        const minValue = this.values.pop();
+        // heapify down the swapped element so that the min heap properties
+        // are maintained
+        this.heapifyDown();
+        
+        return minValue;
+    }
+
+    getSmallChild = (idx) => {
+        if (this.isLeaf(idx)) return this;
+
+        if (((idx*2)+3)>this.values.length) return (idx*2) + 1;
+
+        if (this.values[this.leftChild(idx)]
+            < this.values[this.rightChild(idx)]) {
+            return this.leftChild(idx);
+        } else return this.rightChild(idx);
+    }
+
+    /**
+     * Helper function for removeMin
+     * @returns {Min Heap} (Min) Heapified array
+     */
+    heapifyDown = () => {
+        let currentIdx = 0;
+        let childIdx = this.getSmallChild(currentIdx);
+
+        console.log(this.values[currentIdx], this.values[childIdx], "hello");
+        while (this.values[currentIdx] > this.values[childIdx] 
+                || !childIdx) {
+            if (!childIdx) break;
+            this.swap(currentIdx, childIdx);
+            [currentIdx, childIdx] = [childIdx, this.getSmallChild(childIdx)];
+        }
+
+        return this;
+    }
 }
+
+let newMinHeap = new MinHeap();
+console.log(newMinHeap.values);
+newMinHeap.add(4);
+console.log(newMinHeap.values);
+newMinHeap.add(5);
+console.log(newMinHeap.values);
+newMinHeap.add(2);
+console.log(newMinHeap.values);
+console.log(newMinHeap.removeMin());
+console.log(newMinHeap.values);
+newMinHeap.add(7);
+console.log(newMinHeap.values);
+newMinHeap.add(21);
+console.log(newMinHeap.values);
+newMinHeap.add(1);
+console.log(newMinHeap.values);
+newMinHeap.removeMin();
+console.log(newMinHeap.values);
+newMinHeap.removeMin();
+console.log(newMinHeap.values);
+newMinHeap.removeMin();
+console.log(newMinHeap.values);
+newMinHeap.removeMin();
+console.log(newMinHeap.values);
+newMinHeap.removeMin();
+console.log(newMinHeap.values);
