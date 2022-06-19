717d62176ee9a52a1ac31b56b4fbe06edba02bcd
brendobrendo <brendan.smith903@gmail.com>
2022-06-18 18:12:36 -0700

    ValidParentheses Completed

--- Grind75/ValidParentheses.js
+++ Grind75/ValidParentheses.js
@@ -1,7 +1,5 @@
 var isValid = function (s) {
     // Edge Cases
-    if (s.length === 1) return false
-
     if (s.length % 2 !== 0) return false
 
     // Initialize an empty stack
@@ -10,11 +8,11 @@ var isValid = function (s) {
 
     // Initialize closed character types
     // Improvement: When I was updating the hash table, didn’t explain what I was doing
-    let closedCharacters = { '}': '{', ')': '(', '[': ']' }
+    let closedCharacters = { '}':'{', ')':'(', ']':'[' }
 
     // Improvement: While I was coding this, didn’t talk through what I was coding
     for (let i = 0; i < s.length; i++) {
-        if (closedCharacters.hasOwnProperty(s[i])) {
+        if (!closedCharacters.hasOwnProperty(s[i])) {
             stack.push(s[i]);
         } else {
             if (closedCharacters[s[i]] === stack[stack.length - 1]) {
@@ -23,10 +21,11 @@ var isValid = function (s) {
                 return false
             }
         };
-        return (stack === []);
-
     }
+    return (stack.length === 0);
 }
 
-stack = []
-console.log(stack.length===0);
+console.log(isValid('()')) // true
+console.log(isValid('()(')) // false
+console.log(isValid('(){}')) // true
+console.log(isValid('()[]{}')) // true
