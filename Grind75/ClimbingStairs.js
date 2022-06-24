f70af8254a31a16c83c76faefb4bfe2e3878ea33
brendobrendo <brendan.smith903@gmail.com>
2022-06-24 16:29:03 -0700

    Dynamic Programming and memoization

--- Grind75/ClimbingStairs.js
+++ Grind75/ClimbingStairs.js
@@ -17,7 +17,34 @@ let climbingStairs = (n) => {
 }
 
 let climbingStairsRecursive = (n) => {
-    
+
 }
 
 console.log(climbingStairs(6));
+
+/**
+ * POST MORTEM
+ * Dynamic Problem Solving - Taking a big and sophisticated problem and
+ * breaking it down into smaller, solvable problems. Perfect for recursion.
+ * 
+ * When you think about solving this problem for a small number of steps, finding
+ * the solution is pretty simple- for 0 steps there is 1 distinct way, for 1 step
+ * there is also 1 distinct way, for 2 steps there is 2 distinct ways and yada, yada, yada.
+ * 
+ * However, this problem quickly gets out of hand as the number of distinct possibilities seems
+ * to grow exponentially.
+ * 
+ * To apply a dynamic problem solving solution, we know we could find the solution if we knew 
+ * the number of distinct step patterns for one step and two steps below the number of steps
+ * we're currently solving for.
+ * 
+ * It would then be pretty simple, and seemingly elegant just to apply recursion to our problem.
+ * Setting the base cases for when n=0 (return 1) and n=1 (return 1). Our recursive case would then 
+ * call our function twice- once for n-1 and once for n-2.
+ * 
+ * The one drawback of this approach is it quickly gets to be too many calls for event the computer.
+ * As n gets larger, the amount of calls grows by a 2^n relationship.
+ * 
+ * We can get around this by applying either a memoization technique (if we want to use a hashmap/object)
+ * Or an array (where we can just store the relevant value in an index position and therefore access it in constant time)
+ */
